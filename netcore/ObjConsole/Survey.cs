1088db8d59cc51d4d00cb7918ffd18af9416129b
Teerachai Laothong <tlaothong@gmail.com>
2018-10-31 11:30:29 +0700

    Added Survey and diagram

---
+++ netcore/ObjConsole/Survey.cs
@@ -0,0 +1,26 @@
+public class HomeSample
+{
+    public Residential Residence { get; set; }
+    public Agriculture Agriculture { get; set; }
+    public Commercial Commercial { get; set; }
+}
+
+public class Residential
+{
+    
+}
+
+public class Agriculture
+{
+    public Rice RicePlant { get; set; }
+}
+
+public class Rice
+{
+    
+}
+
+public class Commercial
+{
+    
+}
