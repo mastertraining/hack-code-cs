353747cd7bdddedfd77a28ff8352c98f4363a88c
Teerachai Laothong <tlaothong@gmail.com>
2018-10-31 11:25:07 +0700

    Added ObjGraph example in netcore

---
+++ netcore/ObjConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Order
+    {
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public OrderDetail[] Details { get; set; }
+    }
+
+    public class OrderDetail
+    {        
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
